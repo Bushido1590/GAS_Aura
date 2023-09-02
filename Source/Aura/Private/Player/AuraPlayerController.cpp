6421b7efdde8b8f1cf43bf7702fe3d81d7915ab4
Steven Busby <bushido1590@gmail.com>
2023-09-01 17:43:44 -0700

    added AuraPlayerController setup an inputmappingcontext

---
+++ Source/Aura/Private/Player/AuraPlayerController.cpp
@@ -0,0 +1,28 @@
+// Copyright Haboob Studios
+
+
+#include "Player/AuraPlayerController.h"
+#include "EnhancedInputSubsystems.h"
+
+AAuraPlayerController::AAuraPlayerController()
+{
+	bReplicates = true;
+}
+
+void AAuraPlayerController::BeginPlay()
+{
+	Super::BeginPlay();
+	check(AuraContext);
+
+	UEnhancedInputLocalPlayerSubsystem* Subsystem = ULocalPlayer::GetSubsystem<UEnhancedInputLocalPlayerSubsystem>(GetLocalPlayer());
+	check(Subsystem);
+	Subsystem->AddMappingContext(AuraContext, 0);
+
+	bShowMouseCursor = true;
+	DefaultMouseCursor = EMouseCursor::Default;
+
+	FInputModeGameAndUI InputModeDate;
+	InputModeDate.SetLockMouseToViewportBehavior(EMouseLockMode::DoNotLock);
+	InputModeDate.SetHideCursorDuringCapture(false);
+	SetInputMode(InputModeDate);
+}
