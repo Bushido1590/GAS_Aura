9678393e4992666511a91a90fb97665f38134bc8
Steven Busby <bushido1590@gmail.com>
2023-09-03 21:07:37 -0700

    Added AuraUserWidget Added AuraWidgetController

---
+++ Source/Aura/Private/UI/Widget/AuraUserWidget.cpp
@@ -0,0 +1,10 @@
+// Copyright Haboob Studios
+
+
+#include "UI/Widget/AuraUserWidget.h"
+
+void UAuraUserWidget::SetWidgetController(UObject* InWidgetController)
+{
+	WidgetController = InWidgetController;
+	WidgetControllerSet();
+}
