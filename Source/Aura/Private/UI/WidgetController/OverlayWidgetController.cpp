b6f2d50dc49632cc1466f2846b218305cf0d9259
Steven Busby <bushido1590@gmail.com>
2023-09-07 20:14:31 -0700

    Added Message Tags to Potions and crystals
    Created template to GetDataTableRowByTag

--- Source/Aura/Private/UI/WidgetController/OverlayWidgetController.cpp
+++ Source/Aura/Private/UI/WidgetController/OverlayWidgetController.cpp
@@ -35,13 +35,14 @@ void UOverlayWidgetController::BlindCallbacksToDependencies()
 		AuraAttributeSet->GetMaxManaAttribute()).AddUObject(this, &UOverlayWidgetController::MaxManaChanged);
 
 	Cast<UAuraAbilitySystemComponent>(AbilitySystemComponent)->EffectAssetTags.AddLambda(
-		[](const FGameplayTagContainer& AssetTags)
+		[this](const FGameplayTagContainer& AssetTags)
 		{
 			for (const FGameplayTag& Tag : AssetTags)
 			{
 				const FString Msg = FString::Printf(TEXT("Ge Tag: %s"), *Tag.ToString()); 
 				GEngine->AddOnScreenDebugMessage(-1, 8.f, FColor::Blue, Msg);
-		
+
+				FUIWidgetRow* Row = GetDataTableRowByTag<FUIWidgetRow>(MessageWidgetDataTable, Tag);
 			}
 		}
 	);
